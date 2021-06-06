dd9255cb17613c779534da3426215142c5be2ea0
PersiMersi03 <82722197+PersiMersi03@users.noreply.github.com>
2021-06-06 15:22:19 +0300

    Add files via upload

--- Project Planner/src/project/Team.java
+++ Project Planner/src/project/Team.java
@@ -7,10 +7,15 @@ import java.util.TreeSet;
 
 public class Team {
 
-	private static File flTeam = new File("src/project/Team.txt");
+	private static File flTeam = new File("src/project/Team.txt"); // File-type static parameter
 
+	// The addMember method creates a TreeSet with names. A String is passed to the
+	// method and is later added to the set.
+	// All the stored names in the set are then written into a file by a
+	// PrintWriter.
 	public static void addMember(String name) {
-		if(name.length() == 0) return;
+		if (name.length() == 0)
+			return;
 		TreeSet<String> set = returnTeam();
 		set.add(name);
 
@@ -25,8 +30,13 @@ public class Team {
 		}
 	}
 
+	// The removeMember method creates a TreeSet with names. A string name is passed
+	// to the method.
+	// All the stored names in the set, which do not equal name, are then written
+	// into a file by a PrintWriter.
 	public static void removeMember(String name) {
-		if(name.length() == 0) return;
+		if (name.length() == 0)
+			return;
 		TreeSet<String> set = returnTeam();
 
 		try {
@@ -42,8 +52,10 @@ public class Team {
 		}
 	}
 
+	// The returnTeam method uses a TreeSet to store the names alphabetically. The
+	// file is crawled with a while and each new line is added to the set.
 	public static TreeSet<String> returnTeam() {
-		TreeSet<String> set = new TreeSet<String>(); // TreeSet for collecting the names sorted alphabetically
+		TreeSet<String> set = new TreeSet<String>(); //
 		try {
 			try (Scanner sc = new Scanner(flTeam);) {
 				while (sc.hasNext()) {
@@ -56,6 +68,9 @@ public class Team {
 		return set;
 	}
 
+	// The fileToString method creates a string, goes through the file and
+	// concatenates each new line to the string. The resulting string is then
+	// returned.
 	public static String fileToString() {
 		String s = "";
 		try {
