d85d8d5e82fc1da26e7e7988857ab0922d392063
PersiMersi03 <82722197+PersiMersi03@users.noreply.github.com>
2021-06-06 16:19:45 +0300

    Add files via upload

--- Project Planner/src/project/Planner.java
+++ Project Planner/src/project/Planner.java
@@ -28,43 +28,51 @@ import javax.swing.border.Border;
 import javax.swing.table.AbstractTableModel;
 
 @SuppressWarnings("serial")
-public class Planner extends JFrame {
+public class Planner extends JFrame { // The Planner class extends JFrame
 
 	static JTextArea txtPeopleInTeam = new JTextArea();
 	static JPanel pnl = new JPanel();
-
 	AssignmentTableModel tblModel;
+	// Parameters of the Planner class
 
-	public Planner() {
+	public Planner() { // Public constructor
 
 		super("Planner");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pnl.setLayout(null);
 		pnl.setBackground(Color.white);
+		// Setting layout and background to the panel
 
 		JLabel lblTeam = new JLabel("<html> <font color='red'>Team: </font></html>");
 		lblTeam.setFont(lblTeam.getFont().deriveFont(16.0f));
 		lblTeam.setBounds(30, 25, 50, 50);
 		pnl.add(lblTeam);
+		// Creating a label with the name "Team", colored red, setting its font and
+		// bounds, and adding it to the panel
 
 		Border blackline = BorderFactory.createLineBorder(Color.black);
 
-		setTeam();
+		setTeam(); // Invoking the setTeam method
 		txtPeopleInTeam.setEditable(false);
 		txtPeopleInTeam.setLineWrap(true);
 		txtPeopleInTeam.setWrapStyleWord(true);
 		txtPeopleInTeam.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
 		txtPeopleInTeam.setBorder(blackline);
+		// Setting editability, LineWrap, WrapStyleWord, font and border to the
+		// txtPeopleInTeam TextArea
+
 		JScrollPane scrp = new JScrollPane(txtPeopleInTeam);
 		scrp.setBounds(90, 26, 500, 50);
 		pnl.add(scrp);
+		// Creating a JScrollPane, setting its bounds, and adding it to the panel
 
-		// button for adding new member
 		JButton btnAddNewMember = new JButton("Add New Member");
 		btnAddNewMember.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
 		btnAddNewMember.setBackground(Color.ORANGE);
 		btnAddNewMember.setBorder(blackline);
 		btnAddNewMember.setBounds(600, 25, 175, 50);
+		// Creating a button for adding new member, setting its font, background,
+		// border, and bounds
 		btnAddNewMember.addActionListener(new ActionListener() {
 
 			@Override
@@ -78,8 +86,13 @@ public class Planner extends JFrame {
 			}
 		});
 		pnl.add(btnAddNewMember);
+		// Adding ActionListener to the btnAddNewMember button. The button creates a
+		// JDialog, of which we set the size, location and visibility. To the dialog
+		// window is then passed an AddNewMember-type Object.
+		// The button is then added to the panel.
 
-		// button for adding new member
+		// Creating a button for removing a member, setting its font, background,
+		// border, and bounds
 		JButton btnRemoveMember = new JButton("Remove Member");
 		btnRemoveMember.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
 		btnRemoveMember.setBackground(Color.ORANGE);
@@ -98,6 +111,10 @@ public class Planner extends JFrame {
 			}
 		});
 		pnl.add(btnRemoveMember);
+		// Adding ActionListener to the btnRemoveMember button. The button creates a
+		// JDialog, of which we set the size, location and visibility. To the dialog
+		// window is then passed an RemoveMember-type Object.
+		// The button is then added to the panel.
 
 		tblModel = new AssignmentTableModel(loadFromFile());
 		JTable table = new JTable(tblModel);
@@ -105,6 +122,7 @@ public class Planner extends JFrame {
 		scrollPane.setBounds(10, 100, 960, 100);
 		table.setFillsViewportHeight(true);
 		add(scrollPane);
+		// Creating a new table using the TableModel interface.
 
 		JButton addAssignment = new JButton("Add New Assignment");
 		addAssignment.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
@@ -119,6 +137,10 @@ public class Planner extends JFrame {
 			}
 		});
 		pnl.add(addAssignment);
+		// Creating the addAssignment button for adding new assignments, setting its
+		// font, background, border, and bounds. Adding an action listener to it, which
+		// executes a AddNewAssignment-type dialog window, and then adding it to the
+		// panel.
 
 		JButton btnRemoveAss = new JButton("Remove Assignment");
 		btnRemoveAss.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
@@ -138,7 +160,12 @@ public class Planner extends JFrame {
 			}
 		});
 		pnl.add(btnRemoveAss);
-		
+		// Creating the btnRemoveAss button for removing assignments, setting its
+		// font, background, border, and bounds. Adding an action listener to it, which
+		// removes from the table the selected from the user row, and then adding it to
+		// the
+		// panel.
+
 		JButton btnChangeStatus = new JButton("Change Status");
 		btnChangeStatus.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
 		btnChangeStatus.setBackground(Color.ORANGE);
@@ -159,31 +186,22 @@ public class Planner extends JFrame {
 		});
 		pnl.add(btnChangeStatus);
 
-//		JButton btnCheckProgress = new JButton("Check progress");
-//		btnCheckProgress.setFont(txtPeopleInTeam.getFont().deriveFont(16.0f));
-//		btnCheckProgress.setBackground(Color.ORANGE);
-//		btnCheckProgress.setBorder(blackline);
-//		btnCheckProgress.setBounds(650, 230, 175, 50);
-//		btnCheckProgress.addActionListener(new ActionListener() {
-//
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				JDialog dlgCheckProgress = new JDialog(Planner.this, "Check Progress", true);
-//				dlgCheckProgress.setSize(825, 375);
-//				dlgCheckProgress.setLocationRelativeTo(null);
-//				dlgCheckProgress.add(new Progress());
-//				dlgCheckProgress.setVisible(true);
-//
-//			}
-//
-//		});
-//		pnl.add(btnCheckProgress);
+		// Creating the btnChangeStatus button for changing status of an assignment,
+		// setting its
+		// font, background, border, and bounds. Adding an action listener to it, which
+		// changes the status of the chosen by the user assignment, and then adding it
+		// to the panel.
 
 		this.add(pnl);
 		this.setSize(1000, 350);
 		this.setVisible(true);
+		// Adding the panel to the Planner, setting the size of the frame and its
+		// visibility.
 	}
 
+	// The setTeam method creates an empty string and concatenates each name from
+	// the set with the names to it. It then sets the text to the TextArea to be
+	// this string.
 	public static void setTeam() {
 		String team = "";
 		TreeSet<String> set = Team.returnTeam();
@@ -200,6 +218,10 @@ public class Planner extends JFrame {
 
 	}
 
+	// The AssignmentTableModel class extends AbstractTableModel. It contains two
+	// arrays, used for creating the column names in the table. We than have a
+	// couple of methods for getting the number of rows and columns in a table,
+	// getting a name of a column, getting an object in a given place in the table.
 	class AssignmentTableModel extends AbstractTableModel {
 		Class<?>[] columnClass = new Class[] { String.class, String.class, LocalDate.class, LocalDate.class,
 				Boolean.class };
@@ -249,19 +271,23 @@ public class Planner extends JFrame {
 			case 3:
 				return list.get(rowIndex).getDueDate();
 			case 4:
-				return list.get(rowIndex).isStatus()? "Completed" : "Uncompleted";
+				return list.get(rowIndex).isStatus() ? "Completed" : "Uncompleted";
 			}
 			return "";
 		}
 
 	}
 
+	// To the addToTable method is passed an Assignment-type object, which is added
+	// to the table. The table is then saved.
 	public void addToTable(Assignment a) {
 		tblModel.list.add(a);
 		tblModel.fireTableDataChanged();
 		save(tblModel.list);
 	}
 
+	// The loadFromFile method uses a file with the list of assignments to create an
+	// ArrayList<String>, which is then returned.
 	public static List<Assignment> loadFromFile() {
 		ArrayList<Assignment> list = new ArrayList<Assignment>();
 		DateFormat DF = new SimpleDateFormat("yyyy-MM-dd");
@@ -271,7 +297,7 @@ public class Planner extends JFrame {
 					try {
 						String[] ass = input.nextLine().split("\t");
 						Assignment assignment = new Assignment(ass[0], ass[1], convert(DF.parse(ass[2])),
-								convert(DF.parse(ass[3])), ass[4].equals("true") ? true:false);
+								convert(DF.parse(ass[3])), ass[4].equals("true") ? true : false);
 						list.add(assignment);
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -284,10 +310,13 @@ public class Planner extends JFrame {
 		return list;
 	}
 
+	// The convert method converts a given Data-type object to a Local-type object.
 	public static LocalDate convert(Date dateToConvert) {
 		return new java.sql.Date(dateToConvert.getTime()).toLocalDate();
 	}
 
+	// To the save method is passed a List of assignments. The list is then printed
+	// with a for-each statement into a file.
 	public void save(List<Assignment> list) {
 		File flAssignments = new File("src/project/Assignments.txt");
 
